0a808c9e791d65cad29cd7f89f49654c437b9b5b
monikapsat6 <psat6@cloudeducation.net>
2021-05-18 12:16:32 +0200

    primer commit, prueba añadido de ship order a la view stock move op

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cloudalia_module_misc",
+
+    'summary': """
+        Modulo Cloudalia.""",
+
+    'description': """Cloudalia Educación odoo module.
+    """,
+
+    'author': "Cloudalia Educacion",
+
+    'website': "http://www.cloudaliaeducacion.com",
+
+    'category': 'Technical Settings',
+
+    'version': '11.0.0.1',
+
+    'depends': ['base', 'stock', 'cloudedu_mods'],
+
+    'data': [
+        'views/stock_views.xml',
+        #'views/views.xml',
+    ],
+}
---
+++ models/stock.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class stockpicking(models.Model):
+    _inherit = 'stock.picking'
+
+    purchase_ship_order = fields.Char(compute="_get_ship_order")
+
+    @api.depends('purchase_id')
+    def _get_ship_order(self):
+
+        for record in self:
+            record.purchase_ship_order = record.purchase_id.ship_order
+
+
+class stockmove(models.Model):
+    _inherit = 'stock.move'
+
+    ship_order_move = fields.Char(compute="_get_purchase_ship_order")
+
+    @api.depends('default_picking_id')
+    def _get_purchase_ship_order(self):
+        for record in self:
+            record.ship_order_move = record.env['stock.picking'].browse(
+                record.default_picking_id.purchase_ship_order)
