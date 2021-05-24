3aad6e1bd41d551a4b8a6a334fef1ecb9c7bdcc1
monikapsat6 <psat6@cloudeducation.net>
2021-05-24 09:15:39 +0200

    prueba error import

--- models/stock.py
+++ models/stock.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 
 class stockpicking(models.Model):
