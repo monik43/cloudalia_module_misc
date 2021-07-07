8a4a8d28d6d92855868b05f870fbe2dd24cf86e8
monik43 <79196412+monik43@users.noreply.github.com>
2021-07-07 12:10:40 +0200

    Update res_users.py

--- models/res_users.py
+++ models/res_users.py
@@ -5,13 +5,13 @@ from odoo import models, fields, api, _
 class resusers(models.Model):
     _inherit = 'res.users'
 
-    mobile = fields.Char()
-    vat = fields.Char()
-    street = fields.Char()
-    street2 = fields.Char()
-    zip = fields.Char()
-    city = fields.Char()
+    mobile = fields.Char(store=True)
+    vat = fields.Char(store=True)
+    street = fields.Char(store=True)
+    street2 = fields.Char(store=True)
+    zip = fields.Char(store=True)
+    city = fields.Char(store=True)
     state_id = fields.Many2one(
-        "res.country.state", string='State')
+        "res.country.state", string='State', store=True)
     country_id = fields.Many2one('res.country', string='Country')
-    escola = fields.Char()
+    escola = fields.Char(store=True)
